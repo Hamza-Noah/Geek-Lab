b383f1d3ffb99a99a978f368664ff8f5348adf79
Hamza.Noah <hamza.nouh97@gmail.com>
2024-02-12 06:27:58 +0200

    finished the sections components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar/Navbar";
+import Sections from "./components/Sections/Sections";
 
 export default function App() {
   return (
     <>
     <div className="container-fluid">
       <Navbar />
+      <Sections />
+      <div className="d-flex">
+
+      </div>
     </div>
     </>
   );
---
+++ src/components/Sections/Sections.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import styles from "./section.module.css";
+
+export default function Sections() {
+
+    const [soon, setsoon] = useState(true);
+
+  return (
+    <section className={`${styles.sections} d-flex`}>
+        <div className={`${soon ? styles.soon : ""}`}><h2>Trading</h2></div>
+        <div className={`${soon ? styles.soon : ""}`}><h2>Automation</h2></div>
+        <div className={`${soon ? styles.soon : ""}`}><h2>Portfolio</h2></div>
+        <div className={styles.active}><h2>Alerts</h2></div>
+        <div><h2>Training</h2></div>
+    </section>
+  )
+}
