6d4e3defd4e8d51e420c5a31b441971dcb341ca8
Hamza.Noah <hamza.nouh97@gmail.com>
2024-02-13 01:37:23 +0200

    activated the container in the app

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,17 @@ export default function App() {
       <div className="container-fluid">
         <Navbar />
         <Sections />
-        <div className={`${styles.content} d-flex}`}>
-          <div className={styles.filter}>
-            <Filter />
+        <div className={`${styles.content} container`}>
+          <div className="row">
+            <div className={`${styles.filter} col-md-4`}>
+              <Filter />
+            </div>
+            <div className={`col-md-8 bg-danger`}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
+              ut debitis voluptatum reiciendis distinctio! Aliquid eligendi
+              quibusdam atque expedita eum alias nobis nulla ipsam odit maiores
+              vitae, iure laborum tempora.
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/Data/Data.tsx
@@ -0,0 +1,5 @@
+import styles from "./Data.module.css";
+
+export default function Data() {
+  return <div>Data</div>;
+}
