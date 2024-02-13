034a672d8a164a66cf1c58236cb15592c8ba840f
Hamza.Noah <hamza.nouh97@gmail.com>
2024-02-13 02:59:16 +0200

    added the data component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Sections from "./components/Sections/Sections";
 import Filter from "./components/Filter/Filter";
 import styles from "./App.module.css";
+import Data from "./components/Data/Data";
 
 export default function App() {
   return (
@@ -9,16 +10,13 @@ export default function App() {
       <div className="container-fluid">
         <Navbar />
         <Sections />
-        <div className={`${styles.content} container`}>
+        <div className={`${styles.content}`}>
           <div className="row">
             <div className={`${styles.filter} col-md-4`}>
               <Filter />
             </div>
-            <div className={`col-md-8 bg-danger`}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-              ut debitis voluptatum reiciendis distinctio! Aliquid eligendi
-              quibusdam atque expedita eum alias nobis nulla ipsam odit maiores
-              vitae, iure laborum tempora.
+            <div className={`col`}>
+              <Data />
             </div>
           </div>
         </div>
--- src/components/Data/Data.tsx
+++ src/components/Data/Data.tsx
@@ -1,5 +1,57 @@
 import styles from "./Data.module.css";
+import dataImage1 from "../../assets/images/data-1.svg";
+import dataImage2 from "../../assets/images/data-2.svg";
+import dataImage3 from "../../assets/images/data-3.svg";
+import dataImage4 from "../../assets/images/data-4.svg";
 
 export default function Data() {
-  return <div>Data</div>;
+  const dataAlets = [
+    "X company released a short report on $XYZ, High IV option sales opps",
+    "$XYZ just announced an acquisition of $ABC at $X. This is an arbitrage opportunity, with the max gain being %X if the deal closes, but the possible risk is %Y  if the deal fails.",
+    "$XYZ just announced an acquisition of $ABC at $X. This is an arbitrage opportunity, with the max gain being %X if the deal closes, but the possible risk is %Y  if the deal fails.",
+    "$XYZ just announced an acquisition of $ABC at $X. This is an arbitrage opportunity, with the max gain being %X if the deal closes, but the possible risk is %Y  if the deal fails.",
+    "$XYZ just announced an acquisition of $ABC at $X. This is an arbitrage opportunity, with the max gain being %X if the deal closes, but the possible risk is %Y  if the deal fails.",
+    "$XYZ just announced an acquisition of $ABC at $X. This is an arbitrage opportunity, with the max gain being %X if the deal closes, but the possible risk is %Y  if the deal fails.",
+  ];
+  return (
+    <section className={styles.data}>
+      <div className={styles["data-info"]}>
+        <div className="d-flex justify-content-between">
+          <div className={styles["data-stat"]}>
+            <i>
+              <img src={dataImage1} alt="" />
+            </i>
+            <h3>$TSLA</h3>
+          </div>
+          <div className={styles["data-stat"]}>
+            <i>
+              <img src={dataImage2} alt="" />
+            </i>
+            <h3>200 Contracts</h3>
+          </div>
+          <div className={styles["data-stat"]}>
+            <i>
+              <img src={dataImage3} alt="" />
+            </i>
+            <h3>17.2%</h3>
+          </div>
+          <div className={styles["data-stat"]}>
+            <i>
+              <img src={dataImage4} alt="" />
+            </i>
+            <h3>High Risk</h3>
+          </div>
+        </div>
+      </div>
+        <div className={`${styles.alerts}`}>
+          {dataAlets.map((alert) => {
+            return (
+              <div>
+                <p>{alert}</p>
+              </div>
+            );
+          })}
+        </div>
+    </section>
+  );
 }
