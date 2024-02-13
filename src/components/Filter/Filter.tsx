7f4a2a589e99fcc77297d6fc60e31b8805acc097
Hamza.Noah <hamza.nouh97@gmail.com>
2024-02-13 09:58:23 +0200

    added fixex to the filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,14 +1,15 @@
 import styles from "./Filter.module.css";
+import check from "../../assets/images/check.svg";
 
 export default function Filter() {
   return (
     <section className={styles.filter}>
       <h3 className="fw-bolder">Filters</h3>
-      <form action="">
+      <form onSubmit={e=> e.preventDefault()}>
         <div className="filter-option mb-4">
           <label htmlFor="">Industry</label>
           <div className={styles["select-option"]}>
-            <select name="" id="">
+            <select>
               <option value="health-care">Health care.</option>
             </select>
           </div>
@@ -17,7 +18,7 @@ export default function Filter() {
         <div className="filter-option mb-4">
           <label htmlFor="">Market Cap</label>
           <div className={styles["select-option"]}>
-            <select name="" id="">
+            <select>
               <option value="health-care">Large Cap.</option>
             </select>
           </div>
@@ -26,8 +27,12 @@ export default function Filter() {
         <div className="filter-option mb-4">
           <label htmlFor="">Risk</label>
           <div className={styles["select-option"]}>
-            <select name="" id="">
-              <option value="health-care">Insety text here</option>
+            <select>
+              <option value="">Insert Text Here</option>
+              <option value="low risk">Low Risk</option>
+              <option value="low risk">Mid-Risk</option>
+              <option value="low risk">Low Risk</option>
+              <option value="low risk">Option text here</option>
             </select>
           </div>
           <p>Choose something.</p>
